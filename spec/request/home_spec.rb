da26faaef62795a67c7d968580883c57dcc4f4fb
Clarence Boey <clarenceboey@gmail.com>
2014-04-26 13:20:40 -0700

    Home Page Rspec

---
+++ spec/request/home_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "Home Page" do
+  describe "GET /" do
+
+    context "empty database" do
+    before(:each) do
+      visit '/'
+    end
+
+    it "render success" do
+      expect(page.status_code).to be(200)
+    end
+
+    it "populates title" do
+      expect(page).to have_title 'Comments Dashboard'
+    end
+
+    describe "masthead" do
+     it "displays title" do
+        expect(page).to have_selector 'div#title h1', text:'Comments Dashboard'
+      end
+
+      it "displays subtitle" do
+        expect(page).to have_selector 'h2', text:'Read comments from your favorite blogs'
+      end
+    end
+      end
+  end
+end
